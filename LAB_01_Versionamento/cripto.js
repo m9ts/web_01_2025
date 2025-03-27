4b9c2a2bfb02bc4464730719f392373bab6b5262
m9ts <mat3us.gois@gmail.com>
2025-03-27 20:28:10 -0300

    Manipulação de strings

--- LAB_01_Versionamento/cripto.js
+++ LAB_01_Versionamento/cripto.js
@@ -1,1 +1,10 @@
 // em andamento
+
+var V = 'Vasco da Gama';
+
+console.log('String length: ' + V.length);
+console.log('String charAt: ' + V.charAt(2));
+console.log('String charAt w length: ' + V.charAt(V.length - 4));
+console.log('String replace: ' + V.replace('s', '5'));
+console.log('String toUpperCase: ' + V.toUpperCase());
+console.log('String toLowerCase: ' + V.toLowerCase());
