f67b4688001460cfee7bbd5dfa51295c05a760a9
m9ts <mat3us.gois@gmail.com>
2025-04-25 09:35:27 -0300

    Finalizado ex2 do LAB_09

--- LAB_09_Metodos_Static_String/exercises/ex2.ts
+++ LAB_09_Metodos_Static_String/exercises/ex2.ts
@@ -6,12 +6,24 @@ Método formatar que:
 - Aceite um booleano e retorne "Sim"ou "Não"
 */
 
-class Dados{
-    formatar(a: number, b: string): string;
-
-
-    formatar(a: any, b: any): any{
-
+class Dados {
+    static formatar(a: number): string;
+    static formatar(a: Date): string;
+    static formatar (a: boolean): string;
+    
+    static formatar(a: any): any{
+        if (typeof a === 'number'){
+            return a.toFixed(2);
+        }
+        else if (a instanceof Date){
+            return a.toLocaleDateString("pt-BR");
+        }
+        else (typeof a === 'boolean')
+            return a ? "Sim" : "Não";
     }
 }
 
+console.log("Número decimal com duas casas: ", Dados.formatar(123123.909));
+console.log("Data formatada para DD/MM/AAAA: ", Dados.formatar(new Date(2025, 3, 25)));
+console.log("Retornando o booleano true: ", Dados.formatar(true));
+console.log("Retornando o booleano false: ", Dados.formatar(false));
