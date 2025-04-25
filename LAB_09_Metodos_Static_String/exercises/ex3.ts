29c3f10e90610c4b920dc1cd7228c223e6794b40
m9ts <mat3us.gois@gmail.com>
2025-04-25 10:06:28 -0300

    Finalizado LAB_09

--- LAB_09_Metodos_Static_String/exercises/ex3.ts
+++ LAB_09_Metodos_Static_String/exercises/ex3.ts
@@ -4,3 +4,20 @@ Exercício 3: Manipulação Avançada de Strings
 - Método para mostrar a primeira letra de cada palavra de uma oração.
 - Método para remover todas as vogais
 */
+
+class Manipulacao {
+    static contarOcorrencias(texto: string, busca: string){
+        return texto.split(busca).length -1;
+    }
+    static primeirasLetras(texto: string){
+        return texto.split(' ').map(palavra => palavra.charAt(0)).join('');
+
+    }
+    static removerVogais(texto: string): string{
+        return texto.replace(/[aeiouAEIOU]/g, '');
+    }
+}
+
+console.log("Método para contar ocorrências de uma substring: ", Manipulacao.contarOcorrencias("goiabada", "bada"));
+console.log("Método para mostrar a primeira letra de cada palavra de uma oração: ", Manipulacao.primeirasLetras("Megan Fox"));
+console.log("Método para remover todas as vogais: ", Manipulacao.removerVogais("Moqueca"));
