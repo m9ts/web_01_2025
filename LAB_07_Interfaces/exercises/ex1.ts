4d2ef2831d7d4d05762ce8565a662effb8386f35
m9ts <mat3us.gois@gmail.com>
2025-04-25 10:08:32 -0300

    Resolução do professor para o exercício 1 - DateUtils

--- LAB_07_Interfaces/exercises/ex1.ts
+++ LAB_07_Interfaces/exercises/ex1.ts
@@ -6,8 +6,8 @@ Requisitos Mínimos
 Implemente os seguintes métodos:
 formatarData(data: Date, formato: string): string
 Retorna a data formatada conforme o padrão especificado. Por exemplo:
- "dd/mm/yyyy" -> 11/04/2025
- "yyyy-mm-dd" -> 2025-04-11
+- "dd/mm/yyyy" -> 11/04/2025
+- "yyyy-mm-dd" -> 2025-04-11
 diferencaEmDias(data1: Date, data2: Date): number
 Retorna a quantidade de dias entre duas datas.
 adicionarDias(data: Date, dias: number): Date
@@ -25,3 +25,59 @@ console . log ( DateUtils . adicionarDias ( hoje , 10) ) ;
 console . log ( DateUtils . ehFinalDeSemana ( hoje ) ) ;
 console . log ( DateUtils . converterParaISO ( hoje ) ) ;
 */
+
+export class DateUtils{
+
+    formatarData(data: Date, formato: string): string{
+        let dataFormatada:string = ''
+        let sepador = '/'
+
+        let separadores: string[] = formato.split('/')
+        if(separadores.length == 1){
+            separadores = formato.split('-')
+            sepador = '-'
+        }
+        let cont:number = 0
+        for(let sep of separadores){
+            cont ++
+            if(sep == 'DD'){
+                if(data.getDay() < 10){
+                    dataFormatada += '0'
+                }
+                dataFormatada += data.getDay()
+            }
+            else if(sep == 'MM'){
+                if(data.getMonth() < 10){
+                    dataFormatada += '0'
+                }
+                dataFormatada += data.getMonth()
+            }
+            else if(sep == 'YYYY'){
+                dataFormatada += data.getFullYear()
+            }
+
+            if(cont < 3){
+                dataFormatada += sepador
+            }
+        }
+        return dataFormatada
+    }
+
+    diferencaEmDias(dataInicial: Date, dataFinal: Date):number{
+        const diffEmMs = dataFinal.getTime() - dataInicial.getTime();
+        return diffEmMs / (1000 * 60 * 60 * 24);
+    }
+
+    adicionarDias(data: Date, dias: number): Date{
+        data.setDate(data.getDate() + dias)
+        return data
+    }
+
+    ehFinalDeSemana(data: Date): boolean{
+        return data.getDay() >= 6
+    }
+
+    converterParaISO(data: Date): string{
+        return data.toISOString()
+    }
+}
