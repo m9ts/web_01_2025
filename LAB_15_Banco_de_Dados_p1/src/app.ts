91d470cd7a8ad93535c45512966a5bffaaceea60
m9ts <mat3us.gois@gmail.com>
2025-07-03 12:17:21 -0300

    Atualizando os métodos em app.ts

--- LAB_15_Banco_de_Dados_p1/src/app.ts
+++ LAB_15_Banco_de_Dados_p1/src/app.ts
@@ -4,3 +4,5 @@ const repository: ProductRepository = new ProductRepository();
 repository.createTable();
 
 repository.insertProduct("Camiseta", 20.99);
+repository.updateProduct(1, "Camiseta canelada", 40.99);
+repository.deleteProduct(1);
